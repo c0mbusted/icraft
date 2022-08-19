98c804ef11f5e9b9dd2b009ea350df3afb4306fc
c0mbusted <leticiavp1890@gmail.com>
2022-08-19 18:59:06 -0400

    nuevo repositorio

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import { Container, Grow, Grid, Paper, AppBar, TextField, Button } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { getPosts } from '../../actions/posts';
+import Posts from '../Posts/Posts';
+import Form from '../Form/Form';
+import Pagination from '../Pagination';
+import { useHistory, useLocation } from 'react-router-dom'; 
+import ChipInput from 'material-ui-chip-input';
+import { mergeClasses } from '@material-ui/styles';
+import useStyles from './styles';
+
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+} 
+
+const Home = () => {
+    const [currentId, setCurrentId] = useState(null);
+    const dispatch = useDispatch();
+    const query = useQuery();
+    const history = useHistory();
+    const page = query.get('page') || 1;
+    const searchQuery = query.get('searchQuery');
+    const classes = useStyles();
+    
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [currentId, dispatch]);
+
+    return (
+        <Grow in>
+            <Container maxWidth='xl'>
+                <Grid container justify="space-between" alignItems="stretch" spacing={3} className={classes.gridContainer}>
+                    <Grid item xs={12} md={9} sm={6}>
+                        <Posts setCurrentId={setCurrentId}/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <AppBar className={classes.appBarSearch} position="static" color="inherit">
+                            <TextField 
+                                name="search" 
+                                variant="outlined"
+                                label="Search Memories"
+                                fullWidth
+                                value="TEST"
+                                onChange={() => {
+
+                                }}    
+                            />
+                        </AppBar>
+                        <Form currentId={currentId} setCurrentId={setCurrentId} />
+                        <Paper elevation={6}>
+                            <Pagination />
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
+    )
+}
+
+export default Home
+
+
---
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PostDetails = () => {
+    console.log('post details')
+  return (
+    <div>
+      POST DETAILS
+    </div>
+  )
+}
+
+export default PostDetails
